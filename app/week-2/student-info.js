d6b51b73e7757cd102c75cf088b18aa519323d3c
sukhpreetsinghdeol <sukhpreetsinghdeol@outlook.com>
2024-05-19 18:14:36 -0600

    Completed Park 2 Week 2 Assignment
    - Updated app\page.js
    - Added app\week-2\page.js file and followed instructions in the assignment
    - Created StudentInfo component to display name and Github's Repo Link

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+
+  return (
+    <div>
+      <h2> Sukhpreet Singh</h2>
+      <Link href="https://github.com/sukhpreetsinghdeol/cprg306-assignments"> Github Repository Link </Link>
+    </div>
+  );
+}
